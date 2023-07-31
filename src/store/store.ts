726526a48a2a78471e1b78eaa9daf7b2c0bc9816
azamat <dinaev2002@gmail.com>
2023-08-01 00:50:38 +0300

    Updates

--- src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore} from '@reduxjs/toolkit';
-import thunk from "redux-thunk";
-import { heroReducer } from "./reducer";
-
-const rootReducer = heroReducer
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-export default store
